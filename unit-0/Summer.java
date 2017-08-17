3545e0245e8da26a607eb5b71cb864bfcf11add6
Peter Strawn <pstrawn@stfrancishs.org>
2017-08-17 16:51:56 +0000

    Upload demo program

---
+++ unit-0/Summer.java
@@ -0,0 +1,10 @@
+public class Summer
+{
+    public static void main(String args[])
+    {
+        // description of my summer
+        System.out.println("I took a cruise to Alaska this summer.");
+        System.out.print("I also ");
+        System.out.println("went to three concerts.");
+    }
+}
