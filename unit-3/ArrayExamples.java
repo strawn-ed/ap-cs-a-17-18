829f3502df3d7601513ed40e9c9476c15db0578b
Peter Strawn <pstrawn@stfrancishs.org>
2017-11-07 21:02:34 +0000

    Update Array Example code

--- unit-3/ArrayExamples.java
+++ unit-3/ArrayExamples.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class ArrayExamples
 {
     public static void main(String[] args)
@@ -15,27 +13,27 @@ public class ArrayExamples
         int[] scores_in_Java2 = new int[] { 95, 87, 92, 100 };
         
         // method 3
-        int[] scores_in_Java3 = new int[4];     // this line automatically initializes all elements to 0
+        int[] scores_in_Java3 = new int[4];     // initializes all elements to 0
         scores_in_Java3[0] = 95;
         scores_in_Java3[1] = 87;
         scores_in_Java3[2] = 92;
         scores_in_Java3[3] = 100;
-        // scores_in_Java3[4] = 77;    // can't do this - throws an ArrayIndexOutOfBoundsException
+        // scores_in_Java3[4] = 77;     // throws ArrayIndexOutOfBoundsException
         
-        // printing an array
-        System.out.println(scores_in_Java);
-        System.out.println(Arrays.toString(scores_in_Java));
-
         // C-style iteration over an array
-        for (int i = 0; i < scores_in_Java.length; i++)
+        System.out.println(scores_in_C.length);
+        for (int i = 0; i < scores_in_C.length; i++)
         {
-            System.out.println(scores_in_Java[i]);
+            System.out.println(scores_in_C[i]);
         }
         
-        // Java-style iteration over an array with enhanced for loop (similar to what we saw in Python last year)
+        // Java-style iteration over an array
+        // similar to Python's `for num in nums` approach
+        int sum = 0;
         for (int num : scores_in_Java)
         {
-            System.out.println(++num);
+            sum += num;
         }
+        System.out.println((float) sum / scores_in_Java.length);
     }
 }
